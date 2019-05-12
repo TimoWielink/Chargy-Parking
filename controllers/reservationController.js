80af94e5082f8bc5f5bffb7994051f741807514a
Ozcan <isiko@hva.nl>
2019-05-12 22:05:15 +0200

    dayFix_BlokRes
    reservatiesysteem met aan klikbare blokken

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -5,12 +5,20 @@ cryptr = new Cryptr('myTotalySecretKey');
 
 module.exports.insertTime = function (req, res) {
 
-    let begin = req.body.begin;
-    let eind = req.body.eind;
+    // var arrayChecked = new Array(aantalUren);
+
+    var b1 = req.body.b0;
+
+    console.log(req.body.b0);
 
 
-    let statement = "INSERT INTO `time` (begin_time, end_time) VALUES (?, ?)";
-    con.query(statement, [begin, eind], function (error, results, fields) {
+    // var insert = "UPDATE resTime SET t0100 = ?, t0200 = ?, t0300= ?, t0400 = ?, t0500 = ?, t0600 = ?";
+    // con.query(insert, [arrayChecked[0],arrayChecked[1],arrayChecked[2],arrayChecked[3],arrayChecked[4]], function (error) {
+    //
+
+    var insert = "UPDATE resTime SET t0100 = ?";
+    con.query(insert, [b1], function (error) {
+
 
         if (error) throw error;
         console.log("1 record inserted");
@@ -22,4 +30,5 @@ module.exports.insertTime = function (req, res) {
     }, 5000);
 
     res.redirect('/home');
+
 };
