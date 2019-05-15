6e8b1047b52b9cbc75cf584ca7163ee60e7f16c1
oz <ozcanisik23@gmail.com>
2019-05-15 11:32:04 +0200

    ttttest

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -7,18 +7,17 @@ module.exports.insertTime = function (req, res) {
 
     // var arrayChecked = new Array(aantalUren);
 
-    var b1 = req.body.b0;
+    let b = req.body.id;
 
-    console.log(req.body.b0);
+    console.log(b);
 
 
     // var insert = "UPDATE resTime SET t0100 = ?, t0200 = ?, t0300= ?, t0400 = ?, t0500 = ?, t0600 = ?";
     // con.query(insert, [arrayChecked[0],arrayChecked[1],arrayChecked[2],arrayChecked[3],arrayChecked[4]], function (error) {
     //
 
-    var insert = "UPDATE resTime SET t0100 = ?";
-    con.query(insert, [b1], function (error) {
-
+    let insert = "UPDATE resTime SET t0100 = ? WHERE email = 'oz@oz.nl'";
+    con.query(insert, [b], function (error) {
 
         if (error) throw error;
         console.log("1 record inserted");
