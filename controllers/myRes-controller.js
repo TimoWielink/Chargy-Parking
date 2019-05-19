76ae8861f9be2a734f0c7c9d0930f09fa9b79d48
Mathijs Kroese <kroesem003@hva.nl>
2019-05-19 15:48:38 +0200

    Fixed the object returning to myRes to just show "from time" and "To Time".

--- controllers/myRes-controller.js
+++ controllers/myRes-controller.js
@@ -11,44 +11,37 @@ let LocalStorage = require('node-localstorage').LocalStorage,
 let user;
 // Authenticates if credentials are known in DB and correct
 module.exports.getRes = function (req, res) {
-    // let username = localStorage.getItem("user");
     let username = localStorage.getItem("user");
     console.log(username);
 
-
-    // con.query('SELECT from_time FROM resTime WHERE email = ?', [username], function (error, results, rows)
-    // con.query("SELECT json_object('from_time', from_time, 'to_time)', to_time) FROM resTime WHERE email = 'ozcan23'", function(error, results, rows)
-    // con.query("SELECT from_time AS 'From:', to_time AS 'Till:' FROM resTime WHERE email ='oz@oz.nl'", function(error, results, rows)
-
-
     con.query("SELECT * FROM resTime WHERE email = ?", [username], function (error, results, rows) {
-
-        {
             if (error) throw error;
             if (results.length > 0) {
 
-                var resultArray = Object.values(JSON.parse(JSON.stringify(results)));
+                let resultArray = Object.values(JSON.parse(JSON.stringify(results)));
                 resultArray.forEach(function(getString) {
                     console.log(getString);
                     console.log(getString.from_time);
                     console.log(getString.to_time);
 
                     res.render('pages/myRes', {
-                        name: "",
+                        name: username,
                         fromTime: getString.from_time,
                         toTime: getString.to_time
                     })
                 });
 
             } else {
+                let nullFromTime = "No reservation";
+                let nullToTime = "scheduled yet";
                 results = null;
-                console.log("error: maybe null")
+                console.log("error: maybe null");
+                res.render('pages/myRes',{
+                    name: username,
+                    fromTime: nullFromTime,
+                    toTime: nullToTime
+                })
             }
-        }
-
-
-
-
         // give EJS file the data
 
 // Voert elke 5 sec een query uit zodat de connectie open blijft (niet idle gaat)
