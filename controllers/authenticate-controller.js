36222740388f75d4e52cb1036dbd944f9121c314
Mathijs Kroese <kroesem003@hva.nl>
2019-05-19 15:06:07 +0200

    Fixed the object returning to myRes to just show "from time" and "To Time".

--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -1,6 +1,5 @@
 var con = require('./../config/config');
 passwordHash = require('password-hash');
-
 var LocalStorage = require('node-localstorage').LocalStorage,
     localStorage = new LocalStorage('./scratch');
 
@@ -12,12 +11,17 @@ module.exports.authenticate=function(req,res){
 
     con.query('SELECT * FROM users WHERE username = ?',[username], function (error, results, fields) {
         if (results[0] && passwordHash.verify(enteredPassword, results[0].password)) {
-
-            res.redirect('/time');
-
             localStorage.setItem("user", username);
+
+            req.flash("Welcome",username);
+            return res.render('pages/home');
+
+            // res.redirect('/home');
+
             console.log("LocalStorage username = " + localStorage.getItem("user"));
+
         } else {
+
             req.session.error = 'Incorrect username or password';
             res.render('pages/login', { error: req.session.error });
         }
